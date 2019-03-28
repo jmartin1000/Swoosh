5d159c6a6d9013e4e15c69c5dc1054bb0f108705
Jenifer Martin <jenifer.martin@vml.com>
2019-03-28 13:18:23 -0500

    added onclick actions for buttons on league activity

--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -3,7 +3,7 @@ package com.example.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
