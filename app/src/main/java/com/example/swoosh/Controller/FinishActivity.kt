f3c0b801071185485161098a94e412d9151a5b23
Jenifer Martin <jenifer.martin@vml.com>
2019-03-28 13:55:25 -0500

    added finish activity layout

--- app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/FinishActivity.kt
@@ -2,6 +2,7 @@ package com.example.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.swoosh.R
 
 class FinishActivity : AppCompatActivity() {
 
